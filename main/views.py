0f19d548ab352f9b650dfdf4d8d27a7e69364467
Arthur Gartner <Arthur.Gartner@gmail.com>
2022-12-02 19:37:18 -0500

    Added additional views for initial framework setup

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def page_index(request):
+    return render(request=request,
+                  template_name="main/page_index.html")
